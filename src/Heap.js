7f5563009e910b3e3140026062b014784693902e
Farhan Habib <f.habibca@gmail.com>
2022-02-07 09:27:58 -0800

    Adding add method to heap data structure

--- src/Heap.js
+++ src/Heap.js
@@ -13,6 +13,27 @@ class Heap {
 	peek() {
 		return this.#heap[0]();
 	}
+	/**
+	 *
+	 * @param {T} element The element to be added to the heap
+	 * @throws Will throw if the element is undefined
+	 */
+	add(element) {
+		if (element == undefined) {
+			throw "The element can not be undefined"; //throw if undefined
+		}
+		this.#heap.push(element); //insert element to end of heap
+		//heapify up
+		let current = this.#heap.length - 1;
+		let parent = Math.floor((current - 1) / 2);
+
+		while (current > 0 && this.#heap[parent] > this.#heap[current]) {
+			[this.#heap[parent], this.#heap[current]] = [this.#heap[current], this.#heap[parent]];
+			current = parent;
+			parent = Math.floor((current - 1) / 2);
+		}
+	}
+
 	debug() {
 		console.log(this.#heap);
 	}
