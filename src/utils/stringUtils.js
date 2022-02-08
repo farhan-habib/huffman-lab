81222477b89f24ee03ba4f8a51877d7ad7ffcfcc
Farhan Habib <f.habibca@gmail.com>
2022-02-07 18:50:56 -0800

    Creating String utils Class

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -1,5 +1,26 @@
-static class stringUtils {
+stringUtils = class {
 
+
+	//returns frequency of characters in a string
+	static getCharFrequency(str) {
+		let freq = {};
+		for (let i = 0; i < str.length; i++) {
+			let char = str[i];
+			freq[char] = freq[char] ? freq[char] + 1 : 1;
+		}
+		return freq;
+	}
+	static frequencySorter(freqObj) {
+		let freqArr = [];
+		for (let key in freqObj) {
+			freqArr.push({
+				"char": key,
+				"freq": freqObj[key]
+			})
+		}
+		freqArr.sort(function (a, b) { return b.freq - a.freq });
+		return freqArr;
+	}
 
 }
 
