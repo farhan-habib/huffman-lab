ecdc3b7937b95f84e6074be940984c12a2ffc007
Farhan Habib <f.habibca@gmail.com>
2022-02-08 22:10:49 -0800

    trying to create huffman tree pt 1

--- src/index.js
+++ src/index.js
@@ -1,37 +1,55 @@
-const Heap = require("./Heap");
+const { Heap } = require("./Heap");
+const { stringUtils } = require("./utils/stringUtils");
 
 let arguments = process.argv.splice(2);
 
 let options = {}
 
 let argmap = [
-	{flag:"-o",name:"outputFile",length:2},
+	{ flag: "-o", name: "outputFile", length: 2 },
 ]
 
 for (let i = 0; i < arguments.length; i++) {
-for (let j = 0; j < argmap.length; j++) {
-	
-	if(argmap[j].flag == arguments[i]){
-		let arr = arguments.splice(i,argmap[j].length);
-		arr.shift();
-		options[argmap[j].name] = arr;
+	for (let j = 0; j < argmap.length; j++) {
+
+		if (argmap[j].flag == arguments[i]) {
+			let arr = arguments.splice(i, argmap[j].length);
+			arr.shift();
+			options[argmap[j].name] = arr;
+		}
 	}
 }
 
-	
-}
+/**
+let text = "XNSRZFYVFJJJUDGCTZPSQICCCZISIQHKQZOFSIWKMKDVNWUDWLOSVTDTRVYFSKDQAPWVMHTREURYFKPMKQWKTXYTCBSGOVCCKOHV";
+let freqObj = stringUtils.getCharFrequency(text);
+freqObj = stringUtils.frequencySorter(freqObj);
+// console.log(freqObj);
 
-let x = new Heap(function(a,b){return a - b});
+let comparator = function (a, b) {
+	return (a.freq - b.freq);
+}
+let huffmanHeap = new Heap(comparator);
+
+let huffmanNode = class {
+	constructor({ char, freq }) {
+		this.char = char;
+		this.freq = freq;
+	}
+}
+for (let i = 0; i < freqObj.length; i++) {
+	huffmanHeap.add(new huffmanNode(freqObj[i]));
+}
+huffmanHeap.remove();
+// huffmanHeap.debug();
+**/
+
+let x = new Heap((a, b) => { return a - b });
 
 x.add(1);
-x.add(0);
-x.add(3);
-x.add(4);
 x.add(2);
-x.add(11);
-x.add(9);
+x.add(0);
+x.add(29);
+x.add(130)
 x.remove();
-
-console.log("--");
 x.debug();
-console.log(options);
